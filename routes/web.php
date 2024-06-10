8fa7b184d7d7d886f8a3c276b16945a39a85a6df
natthasath <natthasath.sak@gmail.com>
2024-06-10 10:02:06 +0700

    Add Flowbite

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/tailwind', 'tailwind')->name('tailwind');
+Route::view('/flowbite', 'flowbite')->name('flowbite');
